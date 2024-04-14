5ff6a09b349b3825d7365d3e0ad779ef6e0c59c9
Sat Paing Aung <satpaingaung30@gmail.com>
2024-04-14 10:04:08 +0630

    Add project files.

---
+++ README.md
@@ -0,0 +1,4 @@
+Day 1 - SQL Server Connection and Read data from database
+
+-> F9 (breakpoint)
+-> Dataset => datatable => datarow => datacolumn
--- SPADotNetCore.ConsoleApp/Program.cs
+++ SPADotNetCore.ConsoleApp/Program.cs
@@ -1,2 +1,41 @@
 ﻿
+using System.Data;
+using System.Data.SqlClient;
+using System.Text;
+
 Console.WriteLine("Hello, World!");
+//to search package -- nuget.org
+//need to add sqlconnection package
+
+
+
+//DB Connection String Builder
+SqlConnectionStringBuilder DBConnectionStringBuilder = new SqlConnectionStringBuilder();
+DBConnectionStringBuilder.DataSource = "MDYLPT-0001"; //database servername
+DBConnectionStringBuilder.InitialCatalog = "SPADotNetCoreDB"; //database name
+DBConnectionStringBuilder.UserID = "sa";
+DBConnectionStringBuilder.Password = "sa@123";
+
+SqlConnection sqlserverConnection = new SqlConnection(DBConnectionStringBuilder.ConnectionString);
+sqlserverConnection.Open();
+Console.WriteLine("Connection Open");
+string selectquery = "select * from tbl_Blog";
+SqlCommand cmd = new SqlCommand(selectquery, sqlserverConnection);
+SqlDataAdapter sqlDataAdapter = new SqlDataAdapter(cmd);
+DataTable dt = new DataTable();
+sqlDataAdapter.Fill (dt);
+
+sqlserverConnection.Close();
+Console.WriteLine("Connection Close");
+
+//all data show from database
+foreach (DataRow dr in dt.Rows)
+{
+    Console.WriteLine("Blog ID =>" + dr["BlogID"]);
+    Console.WriteLine("Blog Title =>" + dr["BlogTitle"]);
+    Console.WriteLine("Blog Author =>" + dr["BlogAuthor"]);
+    Console.WriteLine("Blog Content =>" + dr["BlogContent"]);
+    Console.WriteLine("--------------------------");
+}
+
+Console.ReadKey();
